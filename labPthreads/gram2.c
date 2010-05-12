425a4bda617f8ece1c1e4f1c72aab64829171238
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-12 05:33:00 +0200

    Modify gram to accept thread as args

--- labPthreads/gram2.c
+++ labPthreads/gram2.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <string.h>
 #include <pthread.h>
-#define NUM_THREADS	2
+//#define NUM_THREADS	2
+int NUM_THREADS = 2;
 
 double **V,**Q;
 int n;
@@ -15,7 +16,7 @@ struct thread_data
 	int j2;
 };
 
-struct thread_data thread_data_array[NUM_THREADS];
+struct thread_data *thread_data_array;
 
 void *gs(void *threadarg);
 double vecNorm(double *,int );
@@ -23,60 +24,64 @@ double scalarProd(double *,double *,int);
 
 
 int main(int argc, char *argv[]) {
-   int i,j,k,time,t;
-   double sigma,temp_norm,norm;
+	int i,j,k,time,t;
+	double sigma,temp_norm,norm;
 	void *status;
-	pthread_t thread[NUM_THREADS];
+	pthread_t *thread;
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-   n = atoi(argv[1]);
-  
-   //Allocate and fill vectors
-   V = (double **)malloc(n*sizeof(double *));
-   Q = (double **)malloc(n*sizeof(double *));
-   for(i=0;i<n;i++){
-      V[i] = (double *)malloc(n*sizeof(double));
-      Q[i] = (double *)malloc(n*sizeof(double));
-      }
+	n = atoi(argv[1]);
+	NUM_THREADS = atoi(argv[2]);
 
-  for (i = 0; i<n; i++)
-	  for(j=0;j<n;j++)
-		  V[i][j] = rand() % 5 + 1;
-    
+	thread_data_array = (struct thread_data*) malloc(NUM_THREADS*sizeof(struct thread_data)) ;
+	thread = (pthread_t*) malloc(NUM_THREADS * sizeof(pthread_t));
+
+	//Allocate and fill vectors
+	V = (double **)malloc(n*sizeof(double *));
+	Q = (double **)malloc(n*sizeof(double *));
+	for(i=0;i<n;i++){
+		V[i] = (double *)malloc(n*sizeof(double));
+		Q[i] = (double *)malloc(n*sizeof(double));
+	}
+
+	for (i = 0; i<n; i++)
+		for(j=0;j<n;j++)
+			V[i][j] = rand() % 5 + 1;
+
 	time=timer();
-   for(i=0;i<n;i++){
-	   
-	   temp_norm = vecNorm(V[i],n);
-	   for (k=0; k<n; k++)
-		   Q[i][k] = V[i][k]/temp_norm;
+	for(i=0;i<n;i++){
 
-	   for(t=0; t<NUM_THREADS-1; t++) {
-		   thread_data_array[t].iter=i;
-		   thread_data_array[t].j1=i+1+(n-i-1)/NUM_THREADS*t;
-		   thread_data_array[t].j2=i+1+(n-i-1)/NUM_THREADS*(t+1);
-		   // printf("%d %d %d\n",t,thread_data_array[t].j1,thread_data_array[t].j2);
-		   pthread_create(&thread[t], &attr, gs, (void *)&thread_data_array[t]);
-		  
-	   }
-	   t=NUM_THREADS-1;
-	   thread_data_array[t].iter=i;
-	   thread_data_array[t].j1=i+1+(n-i-1)/NUM_THREADS*t;
-	   thread_data_array[t].j2=n;
-	   //printf("%d %d %d\n",t,thread_data_array[t].j1,thread_data_array[t].j2);
-       gs((void *)&thread_data_array[t]);
-	   
-	   for(t=0; t<NUM_THREADS-1; t++)
-		   pthread_join(thread[t], &status);
-   }
+		temp_norm = vecNorm(V[i],n);
+		for (k=0; k<n; k++)
+			Q[i][k] = V[i][k]/temp_norm;
+
+		for(t=0; t<NUM_THREADS-1; t++) {
+			thread_data_array[t].iter=i;
+			thread_data_array[t].j1=i+1+(n-i-1)/NUM_THREADS*t;
+			thread_data_array[t].j2=i+1+(n-i-1)/NUM_THREADS*(t+1);
+			// printf("%d %d %d\n",t,thread_data_array[t].j1,thread_data_array[t].j2);
+			pthread_create(&thread[t], &attr, gs, (void *)&thread_data_array[t]);
+
+		}
+		t=NUM_THREADS-1;
+		thread_data_array[t].iter=i;
+		thread_data_array[t].j1=i+1+(n-i-1)/NUM_THREADS*t;
+		thread_data_array[t].j2=n;
+		//printf("%d %d %d\n",t,thread_data_array[t].j1,thread_data_array[t].j2);
+		gs((void *)&thread_data_array[t]);
+
+		for(t=0; t<NUM_THREADS-1; t++)
+			pthread_join(thread[t], &status);
+	}
 
 	time=timer()-time;
-   printf("Elapsed time: %f \n",time/1000000.0);
-	printf("Check orthogonality: %e \n",scalarProd(Q[n/2], Q[n/3], n));
-	
-   return 0;
-   
+	printf("%d %f \n",NUM_THREADS, time/1000000.0);
+	//printf("Check orthogonality: %e \n",scalarProd(Q[n/2], Q[n/3], n));
+
+	return 0;
+
 }
 
 void *gs(void *threadarg){
@@ -85,7 +90,7 @@ void *gs(void *threadarg){
 	struct thread_data *my_data;
 	my_data = (struct thread_data *) threadarg;
 	j1=my_data->j1; j2=my_data->j2; i=my_data->iter;
-	
+
 	for(j=j1;j<j2;j++){
 		sigma = scalarProd(Q[i],V[j],n);
 		for(k=0;k<n;k++)
@@ -94,22 +99,22 @@ void *gs(void *threadarg){
 }
 
 double vecNorm(double *vec,int n){
-   int i;
-   double local_norm = 0;
-   
-   for(i=0;i<n;i++){
-      local_norm+= (vec[i]*vec[i]);
-   }
-   return sqrt(local_norm);
+	int i;
+	double local_norm = 0;
+
+	for(i=0;i<n;i++){
+		local_norm+= (vec[i]*vec[i]);
+	}
+	return sqrt(local_norm);
 }
 
 double scalarProd(double *a,double *b,int n){
-   int i;
-   double scalar =0.0;
-   
-   for(i=0;i<n;i++){
-      scalar +=a[i]*b[i];
-   }
-   
-   return scalar;
+	int i;
+	double scalar =0.0;
+
+	for(i=0;i<n;i++){
+		scalar +=a[i]*b[i];
+	}
+
+	return scalar;
 }
