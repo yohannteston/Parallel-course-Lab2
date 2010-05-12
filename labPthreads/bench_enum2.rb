4cd15bae3b9b9f9c410948c5fc9eed930ea7a311
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-12 04:51:52 +0200

    Add benchmark file for enum1 and 2

---
+++ labPthreads/bench_enum2.rb
@@ -0,0 +1,22 @@
+#!/usr/local/bin/ruby
+
+# Benchmark for enum2
+# For each number of thread launch 20 times the program
+# and keep the least time.
+[1,4,8,12,16,24,32,48,64].each do |n|
+
+	min_time = 10000000 ;
+	min_thread = 0;
+
+	20.times {
+		out = `enum2 #{n}` 
+		out_split = out.split " " ;
+		if(out_split[1].to_f < min_time)
+			min_time = out_split[1].to_f;
+			min_thread = out_split[0];
+	puts "#{min_thread}  #{min_time}" ;
+		end
+	}
+
+	puts "#{min_thread}  #{min_time}" ;
+end
